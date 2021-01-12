dc804b7a46193e7760fc44a8df3a14d95a3e7228
ArinzeJeffrey-droid <nnamenearinze@gmail.com>
2021-01-12 11:54:47 +0100

    removed redux extension middlware from redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,5 @@ const middleware = [thunk];
 export default createStore(
     rootReducer,
     compose(
-        applyMiddleware(...middleware),
-        process.env.NODE_ENV === "development" ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : ""
-    )
+        applyMiddleware(...middleware))
 );
